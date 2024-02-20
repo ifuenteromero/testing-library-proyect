190e7fcbfd82f10cf07ddc3423bb2ea6cd2c04d0
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-02-20 16:50:05 +0100

    Scoping test hooks

--- src/components/auth/AuthButtons.test.js
+++ src/components/auth/AuthButtons.test.js
@@ -1,32 +1,37 @@
 import createServer from '../../tests/createServer';
 
-// createServer() => GET api/user => { user: null }
+describe('when user is not signed in', () => {
+	// createServer() => GET api/user => { user: null }
 
-createServer([
-	{
-		path: '/api/user',
-		response: () => ({ user: null }),
-	},
-]);
+	createServer([
+		{
+			path: '/api/user',
+			response: () => ({ user: null }),
+		},
+	]);
 
-test('when user is not signed in, sign in and sign up are visible ', async () => {
-	console.log('test 1');
+	test('sign in and sign up are visible', async () => {
+		console.log('test 1');
+	});
+	test('sign out is not visible', async () => {
+		console.log('test 2');
+	});
 });
-test('when user is not signed in, sign out is not visible ', async () => {
-	console.log('test 2');
-});
 
-createServer([
-	{
-		path: '/api/user',
-		response: () => ({ user: { id: 3, email: 'test@gmail.com' } }),
-	},
-]);
+describe('when user is signed in', () => {
+	// createServer() => GET api/user => { user: { id: 2, email: test@gmail.com }l }
 
-// createServer() => GET api/user => { user: { id: 2, email: test@gmail.com }l }
-test('when user is signed in, sign in and sign up are not visible ', async () => {
-	console.log('test 3');
-});
-test('when user is signed in, sign out is visible ', async () => {
-	console.log('test 4');
+	createServer([
+		{
+			path: '/api/user',
+			response: () => ({ user: { id: 3, email: 'test@gmail.com' } }),
+		},
+	]);
+
+	test('sign in and sign up are not visible', async () => {
+		console.log('test 3');
+	});
+	test('sign out is visible', async () => {
+		console.log('test 4');
+	});
 });
